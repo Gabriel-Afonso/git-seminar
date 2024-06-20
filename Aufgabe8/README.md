a6164179809292322e0532b9e9053726013dddd7
Gabriel Afonso <gba@ordix.de>
2024-06-20 02:42:18 +0200

    added Aufgabe8

---
+++ Aufgabe8/README.md
@@ -0,0 +1,38 @@
+## Aufgabe: Auf einen alten Commit springen mit Reset
+
+### Ziel:
+
+Ziel dieser Aufgabe ist es den Git-Befehl `reset` kennen und anwenden zu lernen.
+
+### Setup:
+
+Aufgabe mit dem `setup.sh`-Skript einrichten:
+
+```bash
+chmod +x setup.sh
+./setup.sh
+```
+
+Aufgabe im Unterordner "exercise" bearbeiten:
+
+```bash
+cd exercise
+```
+
+Sollte ein Fehler auftreten - einfach das Setup-Skript erneut ausführen:
+
+```bash
+./setup.sh
+```
+
+### Aufgabe:
+
+- Das vorgegebene Repository hat einen Fehler in der aktuellen Version. Der Test `test.sh` scheint nicht richtig durchzulaufen. Beheben Sie den Fehler mithilfe einen neuen Commit zu erstellen.
+- Was ist der unterschied zwischen `git reset --soft <commit-id>` und `git reset --hard <commit-id>`? Wann sollte man welchen Befehl verwenden?
+- Wann sollte `git reset` NICHT verwendet werden? Warum?
+
+### Hilfreiche Ressourcen:
+
+- [https://git-scm.com/docs/git-reset](https://git-scm.com/docs/git-reset)
+- [https://www.atlassian.com/git/tutorials/undoing-changes/git-reset](https://www.atlassian.com/git/tutorials/undoing-changes/git-reset)
+- [https://stackoverflow.com/questions/2530060/in-plain-english-what-does-git-reset-do](https://stackoverflow.com/questions/2530060/in-plain-english-what-does-git-reset-do)
---
+++ Aufgabe8/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# create exercise repository
+rm -rf exercise
+git init exercise
+cd exercise
+
+echo '#! /usr/bin/env bash' > 'test.sh'
+echo 'VERY_IMPORTANT_VARIABLE=true' >> 'test.sh'
+echo 'if [ "$VERY_IMPORTANT_VARIABLE" = false ] ; then' >> 'test.sh'
+echo ' echo "error"' >> 'test.sh'
+echo '  exit 1' >> 'test.sh'
+echo 'else' >> 'test.sh' >> 'test.sh'
+echo '  echo "test passed"' >> 'test.sh'
+echo '  exit 0' >> 'test.sh'
+echo 'fi' >> 'test.sh'
+chmod +x 'test.sh'
+git add "test.sh"
+git commit -m "Add test"
+
+# Datei "file1.txt" mit Inhalt "Text in Datei 1" erstellen und committen
+echo "Text in Datei 1" > file1.txt
+git add file1.txt
+git commit -m "Erste Datei hinzufügen"
+
+# Datei "file2.txt" mit Inhalt "Text in Datei 2" erstellen und committen
+echo "Text in Datei 2" > file2.txt
+git add file2.txt
+git commit -m "Zweite Datei hinzufügen"
+
+# Datei "password.txt" mit Inhalt "password123" erstellen
+echo "password123" > password.txt
+echo "Neuer Text in Datei 1" > file1.txt
+git add *
+git commit -m "Neuer Text in Datei 1"
+
+# Datei "file3.txt" mit Inhalt "Text in Datei 3" erstellen und committen
+echo "Text in Datei 3" > file3.txt
+git add file3.txt
+git commit -m "Dritte Datei hinzufügen"
+
+# Datei "file2.txt" anpassen und committen
+echo "Neuer Text in Datei 2" > file2.txt
+git commit -am "Zweite Datei angepasst"
+
+echo '#! /usr/bin/env bash' > 'test.sh'
+echo 'VERY_IMPORTANT_VARIABLE=false' >> 'test.sh'
+echo 'if [ "$VERY_IMPORTANT_VARIABLE" = false ] ; then' >> 'test.sh'
+echo ' echo "error"' >> 'test.sh'
+echo '  exit 1' >> 'test.sh'
+echo 'else' >> 'test.sh' >> 'test.sh'
+echo '  echo "test passed"' >> 'test.sh'
+echo '  exit 0' >> 'test.sh'
+echo 'fi' >> 'test.sh'
+git add "test.sh"
+git commit -m "changed test"
